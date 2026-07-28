dca0b3a38a5dfe0614b731e766327e131122b171
svitalis123 <vitalis.wilson@yahoo.com>
2026-07-28 23:00:47 +0300

    achanged url

--- src/config/settings/base_storage.py
+++ src/config/settings/base_storage.py
@@ -9,7 +9,8 @@ AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID", default="")
 AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY", default="")
 AWS_S3_REGION_NAME = env("AWS_REGION", default="us-east-1")
 AWS_STORAGE_BUCKET_NAME = env("AWS_BUCKET", default="")
-AWS_S3_CUSTOM_DOMAIN = env("S3_URL", default="")
+_s3_url = env("S3_URL", default="")
+AWS_S3_CUSTOM_DOMAIN = _s3_url.replace("https://", "").replace("http://", "").rstrip("/")
 
 STORAGES = {
     "default": {
